061e62836e65a44c57538a8089e451fd57fae83a
wlandeck <w.landeck@hotmail.com>
2014-11-20 00:26:22 -0800

    Assembly Update

--- IRepository/Properties/AssemblyInfo.cs
+++ IRepository/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+//The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5b376d92-ff0a-4556-824e-3e6712c2a3bd")]
 
 // Version information for an assembly consists of the following four values:
